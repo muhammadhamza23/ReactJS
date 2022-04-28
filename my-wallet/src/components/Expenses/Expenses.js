578c84c60ecc5477e86e524aa7a1f32989b17719
Muhammad Hamza <muhammadhamza.smhn@gmail.com>
2022-04-28 17:21:32 +0500

    created expense list component

--- my-wallet/src/components/Expenses/Expenses.js
+++ my-wallet/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
 function Expenses(props) {
@@ -17,19 +17,6 @@ function Expenses(props) {
 
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0){
-      expensesContent = filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -37,7 +24,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
